346c14781a60845e418319836e108c5238d1f2b9
Michael Heffernan <michael.heffernan.j@gmail.com>
2020-02-02 08:22:13 -0500

    cleaning up comments on layout page

--- src/components/layout.js
+++ src/components/layout.js
@@ -66,39 +66,3 @@ export default ({ children }) => {
     </div>
   )
 }
-
-// const ListLink = props => (
-//   <li style={{ display: `inline-block`, marginRight: `1rem` }}>
-//     <Link to={props.to}>{props.children}</Link>
-//   </li>
-// )
-
-// export default ({ children }) => {
-//   const data = useStaticQuery(
-//     graphql`
-//       query {
-//         site {
-//           siteMetadata {
-//             author
-//           }
-//         }
-//       }
-//     `
-//   )
-//   return (
-//     <div style={{ margin: `3rem auto`, maxWidth: 800, padding: `0 1rem` }}>
-//       <header style={{ marginBottom: `1.5rem` }}>
-//         <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-//           <h3 style={{ display: `inline` }}>{data.site.siteMetadata.author}</h3>
-//         </Link>
-//         <ul style={{ listStyle: `none`, float: `right` }}>
-//           <ListLink to="/portfolio/">Portfolio</ListLink>
-//           <ListLink to="/about/">About</ListLink>
-//           <ListLink to="/contact/">Contact</ListLink>
-//         </ul>
-//       </header>
-//       {children}
-//       <footer>I'm a footer</footer>
-//     </div>
-//   )
-// }
