2dded3d1133301190f57cafc09a48e263ff05831
Michael Heffernan <michael.heffernan.j@gmail.com>
2020-02-02 08:21:11 -0500

    refactored layout to use emotion

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,15 @@
 import React from "react"
+import { css } from "@emotion/core"
 import { Link, useStaticQuery, graphql } from "gatsby"
+import { rhythm } from "../utils/typography"
 
 const ListLink = props => (
-  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+  <li
+    css={css`
+      display: inline-block;
+      margin-right: 1rem;
+    `}
+  >
     <Link to={props.to}>{props.children}</Link>
   </li>
 )
@@ -13,6 +20,7 @@ export default ({ children }) => {
       query {
         site {
           siteMetadata {
+            title
             author
           }
         }
@@ -20,19 +28,77 @@ export default ({ children }) => {
     `
   )
   return (
-    <div style={{ margin: `3rem auto`, maxWidth: 800, padding: `0 1rem` }}>
-      <header style={{ marginBottom: `1.5rem` }}>
-        <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-          <h3 style={{ display: `inline` }}>{data.site.siteMetadata.author}</h3>
-        </Link>
-        <ul style={{ listStyle: `none`, float: `right` }}>
-          <ListLink to="/portfolio/">Portfolio</ListLink>
-          <ListLink to="/about/">About</ListLink>
-          <ListLink to="/contact/">Contact</ListLink>
-        </ul>
-      </header>
+    <div
+      css={css`
+        margin: 0 auto;
+        max-width: 700px;
+        padding: ${rhythm(2)};
+        padding-top: ${rhythm(1.5)};
+      `}
+    >
+      <Link
+        to={`/`}
+        css={css`
+          background-image: none;
+        `}
+      >
+        <h3
+          css={css`
+            margin-bottom: ${rhythm(2)};
+            display: inline-block;
+            font-style: normal;
+            margin-top: 0;
+          `}
+        >
+          {data.site.siteMetadata.author}
+        </h3>
+      </Link>
+      <ul
+        css={css`
+          list-style: none;
+          float: right;
+        `}
+      >
+        <ListLink to="/portfolio/">Portfolio</ListLink>
+        <ListLink to="/about/">About</ListLink>
+      </ul>
       {children}
-      <footer>I'm a footer</footer>
     </div>
   )
 }
+
+// const ListLink = props => (
+//   <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+//     <Link to={props.to}>{props.children}</Link>
+//   </li>
+// )
+
+// export default ({ children }) => {
+//   const data = useStaticQuery(
+//     graphql`
+//       query {
+//         site {
+//           siteMetadata {
+//             author
+//           }
+//         }
+//       }
+//     `
+//   )
+//   return (
+//     <div style={{ margin: `3rem auto`, maxWidth: 800, padding: `0 1rem` }}>
+//       <header style={{ marginBottom: `1.5rem` }}>
+//         <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+//           <h3 style={{ display: `inline` }}>{data.site.siteMetadata.author}</h3>
+//         </Link>
+//         <ul style={{ listStyle: `none`, float: `right` }}>
+//           <ListLink to="/portfolio/">Portfolio</ListLink>
+//           <ListLink to="/about/">About</ListLink>
+//           <ListLink to="/contact/">Contact</ListLink>
+//         </ul>
+//       </header>
+//       {children}
+//       <footer>I'm a footer</footer>
+//     </div>
+//   )
+// }
