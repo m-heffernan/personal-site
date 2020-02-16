7bfbbe4831bc2bc79f4cd8d883b4362c777028f1
Michael Heffernan <michael.heffernan.j@gmail.com>
2020-02-16 15:40:40 -0500

    added image and details to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,35 @@
 import React from "react"
 import Layout from "../components/layout"
+import Img from "gatsby-image"
 
-export default () => (
-  <Layout>
-    <h1>About me</h1>
-    <p>I started my career as a dev ops engineer in New York City. </p>
-  </Layout>
-)
+export default ({ data }) => {
+  return (
+    <Layout>
+      <h1>About</h1>
+      <Img fluid={data.fileName.childImageSharp.fluid} alt="" />
+      <h2>What I Do for a Living</h2>
+      <p>
+        I work as a Customery Delivery consultant specialiazing in API
+        management platforms.
+      </p>
+      <h2>What I Do for Fun</h2>
+      <ul>
+        <li>I ride bikes through New York City</li>
+        <li>Grow veggies</li>
+      </ul>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    fileName: file(relativePath: { eq: "images/about-pic.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 400, quality: 100) {
+          ...GatsbyImageSharpFluid
+          presentationWidth
+        }
+      }
+    }
+  }
+`
